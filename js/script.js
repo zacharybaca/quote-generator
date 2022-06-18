fed4b90306ae8054f25e0ea9c89511e0a6f8a066
Zachary Baca <bacazach@icloud.com>
2022-06-17 23:35:53 -0500

    added objects with quotes and created random quote function

--- js/script.js
+++ js/script.js
@@ -11,13 +11,54 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+let quotes = [
+
+  {
+    quote: 'Believe you can and you are halfway there.',
+    source: 'Theodore Roosevelt',
+    citation: '',
+    year: ''
+  },
+
+  {
+    quote: 'All our dreams can come true, if we have the courage to pursue them.',
+    source: 'Walt Disney',
+    citation: '',
+    year: ''
+  },
+
+  {
+    quote: 'Love your Enemies, for they tell you your Faults.',
+    source: 'Benjamin Franklin',
+    citation: 'Poor Richard\s Almanack',
+    year: '1756'
+  },
+
+  {
+    quote: 'A true Friend is the best Possession',
+    source: 'Benjamin Franklin',
+    citation: 'Poor Richard\s Almanack',
+    year: '1744'
+  },
+
+  {
+    quote: 'The only thing that interferes with my learning is my education.',
+    source: 'Albert Einstein',
+    citation: '',
+    year: ''
+  }
+];
+
 
 
 /***
  * `getRandomQuote` function
 ***/
-
-
+function getRandomQuote(arr) {
+  let randomNumber = Math.floor(Math.random() * arr.length);
+  console.log(randomNumber);
+}
+getRandomQuote(quotes);
 
 /***
  * `printQuote` function
